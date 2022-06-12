7a7df5a34d82469d9a167ca58b878d0853654452
gabrdss <102000237+gabrdss@users.noreply.github.com>
2022-06-12 17:04:08 -0300

    Create-Node.js-Server

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: 'Crush Laranja (Lata) 220 mL',
+      slug: 'crush-laranja-lata-220ml',
+      category: 'Bebidas',
+      image: '/images/33-334569_soda-png.png',
+      price: 2.19,
+      countInStock: 10,
+      brand: 'Crush',
+      rating: 4.8,
+      numreviews: 21,
+      description:
+        'Com sabor irresistível e único, o Refrigerante Crush Laranja faz muito sucesso no mercado. Produzido com água gaseificada, açúcar e suco de laranja é uma excelente opção para matar a sede! Compre Refrigerante Crush Laranja Garrafa e experimente esse sabor inconfundível. Lote: 5632. Validade: 18/12/2022.',
+    },
+    {
+      name: 'Crush Laranja (Pet) 600 mL',
+      slug: 'crush-laranja-pet-600ml',
+      category: 'Bebidas',
+      image: '/images/pngwing.com (13).png',
+      price: 6.95,
+      countInStock: 12,
+      brand: 'Crush',
+      rating: 4.8,
+      numreviews: 16,
+      description:
+        'Com sabor irresistível e único, o Refrigerante Crush Laranja faz muito sucesso no mercado. Produzido com água gaseificada, açúcar e suco de laranja é uma excelente opção para matar a sede! Compre Refrigerante Crush Laranja Garrafa e experimente esse sabor inconfundível. Lote: 5632. Validade: 18/12/2022.',
+    },
+    {
+      name: 'Pão de Forma Tradicional Gardenia 600 g',
+      slug: 'pao-de-forma-gardenia-600g',
+      category: 'Padaria',
+      image: '/images/pngegg (1).png',
+      price: 7.39,
+      countInStock: 25,
+      brand: 'Gardenia',
+      rating: 4.4,
+      numreviews: 32,
+      description:
+        'Versátil, prático e para qualquer momento, trazendo a energia necessária para o seu dia. Perfeito para sanduíches, torradas, receitas para o café da manhã, café da tarde, almoço e jantar. Lote: 1456. Validade: 14/09/2022.',
+    },
+    {
+      name: 'Iogurte Grego Yoplait 100 g',
+      slug: 'iogurte-grego-yoplait-100g',
+      category: 'Laticínios',
+      image: '/images/yogurt-clipart-27944.png',
+      price: 2.17,
+      countInStock: 15,
+      brand: 'Yoplait',
+      rating: 4.3,
+      numreviews: 18,
+      description:
+        'O Iogurte Grego foi inspirado em uma receita de iogurte da Grecia, que trazia uma consistência diferenciada. O Iogurte Grego Danone acredita que pode proporcionar a você um momento de pausa no seu dia para se deliciar com um iogurte super saboroso e cremoso, te trazendo momentos de prazer. Lote: 5698. Validade: 25/12/2022.',
+    },
+  ],
+};
+
+export default data;
