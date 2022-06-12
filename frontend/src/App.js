5a65b68af7c922ec8cd4e18be2ebc9c3c5538123
gabrdss <102000237+gabrdss@users.noreply.github.com>
2022-06-12 00:07:43 -0300

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Alecrim</a>
+      </header>
+      <main>Listar Produtos</main>
+    </div>
+  );
+}
+
+export default App;
