2424b7c9bf77ae958ab6bcea277edb9247d4db29
gabrdss <102000237+gabrdss@users.noreply.github.com>
2022-10-10 00:54:53 -0300

    manage-products

--- backend/utils.js
+++ backend/utils.js
@@ -21,7 +21,7 @@ export const isAuth = (req, res, next) => {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
-        res.status(401).send({ message: 'Token inválido' });
+        res.status(401).send({ message: 'Token Inválido' });
       } else {
         req.user = decode;
         next();
@@ -29,5 +29,13 @@ export const isAuth = (req, res, next) => {
     });
   } else {
     res.status(401).send({ message: 'Sem Token' });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: 'Admin Token Inválido' });
   }
 };
