ff683f1b0274effb1c21f9fc7ee27b77d1569fe5
gabrdss <102000237+gabrdss@users.noreply.github.com>
2022-10-16 19:42:36 -0300

    list-orders

---
+++ frontend/src/screens/OrderListScreen.js
@@ -0,0 +1,130 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import Button from 'react-bootstrap/Button';
+import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { Store } from '../Store';
+import { getError } from '../utils';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        orders: action.payload,
+        loading: false,
+      };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+export default function OrderListScreen() {
+  const navigate = useNavigate();
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/orders`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(err),
+        });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
+
+  return (
+    <div>
+      <Helmet>
+        <title>Pedidos</title>
+      </Helmet>
+      <h1>Pedidos</h1>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USUÁRIO</th>
+              <th>DATA</th>
+              <th>TOTAL</th>
+              <th>PAGO</th>
+              <th>ENTREGUE</th>
+              <th>AÇÕES</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user ? order.user.name : 'USUÁRIO EXCLUÍDO'}</td>
+                <td>
+                  {' '}
+                  {order.createdAt.substring(8, 10)}-
+                  {order.createdAt.substring(5, 7)}-
+                  {order.createdAt.substring(0, 4)}
+                </td>
+                <td>R${order.totalPrice.toFixed(2)}</td>
+                <td>
+                  {order.isPaid
+                    ? order.paidAt.substring(8, 10) +
+                      '-' +
+                      order.paidAt.substring(5, 7) +
+                      '-' +
+                      order.paidAt.substring(0, 4)
+                    : 'Não'}
+                </td>
+                <td>
+                  {order.isDelivered
+                    ? order.deliveredAt.substring(8, 10) +
+                      '-' +
+                      order.deliveredAt.substring(5, 7) +
+                      '-' +
+                      order.deliveredAt.substring(0, 4)
+                    : 'Não'}
+                </td>
+                <td>
+                  {order.isDelivered
+                    ? order.deliveredAt.substring(0, 10)
+                    : 'Não'}
+                </td>
+                <td>
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={() => {
+                      navigate(`/order/${order._id}`);
+                    }}
+                  >
+                    Detalhes
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
